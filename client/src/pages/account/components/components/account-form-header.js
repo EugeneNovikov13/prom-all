14234901e023d8c2df13ab691c0cd75ed89dd2b4
EugeneNovikov13 <eugene.work.4940@gmail.com>
2024-02-05 22:40:17 +0300

    Напоминание об активации добавил

--- client/src/pages/account/components/components/account-form-header.js
+++ client/src/pages/account/components/components/account-form-header.js
@@ -1,10 +1,19 @@
-import { H1 } from '../../../../components';
+import { useSelector } from 'react-redux';
+import { H1, P } from '../../../../components';
 import styled from 'styled-components';
 
 const AccountFormHeaderContainer = ({ className }) => {
+	const userEmailIsActivated = useSelector(state => state.appReducer.user.isActivated);
+
 	return (
 		<div className={className}>
 			<H1 color="var(--white)">Личный кабинет</H1>
+			{!userEmailIsActivated &&
+				<P color={'var(--brand-orange)'} fontSize="12px">
+					* Активируйте аккаунт по ссылке, отправленной на Вашу электронную
+					почту, пожалуйста
+				</P>
+			}
 		</div>
 	);
 };
